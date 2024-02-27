5242650f5ea40e57fc6cc6ae3f76a86e07e1b7d5
Mochamad Syahrial Alzaidan <88835497+syahrialalzaidan@users.noreply.github.com>
2024-02-27 15:48:17 +0700

    fixed: file or plaintext input

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ function App() {
   const [text, setText] = useState("");
   const [key, setKey] = useState("");
   const [column, setColumn] = useState(0);
+  const [isFile, setIsFile] = useState(false);
 
   const inputSection = (type: string) => {
     if (
@@ -36,13 +37,67 @@ function App() {
     ) {
       return (
         <div className="flex flex-col gap-4">
-          <strong>{encrypt ? "Plain Text" : "Ciphertext"}</strong>
-          <textarea
-            className="border border-gray-200 p-4 text-base rounded-lg"
-            placeholder="Enter your text here"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          />
+          <div className="flex gap-2 justify-around">
+            <strong
+              className={`cursor-pointer ${
+                !isFile ? "text-blue-200" : "text-black"
+              }`}
+              onClick={() => {
+                setIsFile(false);
+              }}
+            >
+              {encrypt ? "Plain Text" : "Ciphertext"}
+            </strong>
+            <p>or</p>
+            <strong
+              className={`cursor-pointer ${
+                isFile ? "text-blue-200" : "text-black"
+              }`}
+              onClick={() => {
+                setIsFile(true);
+              }}
+            >
+              File
+            </strong>
+          </div>
+          {!isFile ? (
+            <textarea
+              className="border border-gray-200 min-h-48 p-4 text-base rounded-lg"
+              placeholder="Enter your text here"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+          ) : (
+            <div className="flex items-center justify-center w-full">
+              <label className="flex flex-col items-center justify-center w-full h-48 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg
+                    className="w-8 h-8 mb-4 text-gray-500"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 16"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                    />
+                  </svg>
+                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-gray-400">
+                    PDF
+                  </p>
+                </div>
+                <input id="dropzone-file" type="file" className="hidden" />
+              </label>
+            </div>
+          )}
 
           <strong>Key</strong>
           <textarea
@@ -134,13 +189,67 @@ function App() {
             </div>
           </div>
 
-          <strong>{encrypt ? "Plain Text" : "Ciphertext"}</strong>
-          <textarea
-            className="border border-gray-200 p-4 text-base rounded-lg"
-            placeholder="Enter your text here"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          />
+          <div className="flex gap-2 justify-around">
+            <strong
+              className={`cursor-pointer ${
+                !isFile ? "text-blue-200" : "text-black"
+              }`}
+              onClick={() => {
+                setIsFile(false);
+              }}
+            >
+              {encrypt ? "Plain Text" : "Ciphertext"}
+            </strong>
+            <p>or</p>
+            <strong
+              className={`cursor-pointer ${
+                isFile ? "text-blue-200" : "text-black"
+              }`}
+              onClick={() => {
+                setIsFile(true);
+              }}
+            >
+              File
+            </strong>
+          </div>
+          {!isFile ? (
+            <textarea
+              className="border border-gray-200 min-h-48 p-4 text-base rounded-lg"
+              placeholder="Enter your text here"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+          ) : (
+            <div className="flex items-center justify-center w-full">
+              <label className="flex flex-col items-center justify-center w-full h-48 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg
+                    className="w-8 h-8 mb-4 text-gray-500"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 16"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                    />
+                  </svg>
+                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-gray-400">
+                    PDF
+                  </p>
+                </div>
+                <input id="dropzone-file" type="file" className="hidden" />
+              </label>
+            </div>
+          )}
         </div>
       );
     } else {
@@ -209,18 +318,20 @@ function App() {
   };
 
   return (
-    <div className="bg-blue-200 flex flex-col gap-12 items-center justify-center min-h-screen">
+    <div className="bg-blue-200 flex flex-col gap-12 p-14 items-center justify-center min-h-screen">
       <div className="flex gap-4 text-4xl font-semibold">
         <div
-          className={`cursor-pointer ${encrypt ? "bg-white text-blue-200" : "bg-transparent text-white"
-            } p-4 rounded-lg`}
+          className={`cursor-pointer ${
+            encrypt ? "bg-white text-blue-200" : "bg-transparent text-white"
+          } p-4 rounded-lg`}
           onClick={() => setEncrypt(true)}
         >
           <h1 className="">Encryption</h1>
         </div>
         <div
-          className={`cursor-pointer ${!encrypt ? "bg-white text-blue-200" : "bg-transparent text-white"
-            } p-4 rounded-lg`}
+          className={`cursor-pointer ${
+            !encrypt ? "bg-white text-blue-200" : "bg-transparent text-white"
+          } p-4 rounded-lg`}
           onClick={() => setEncrypt(false)}
         >
           <h1>Decryption</h1>
@@ -239,7 +350,6 @@ function App() {
             <option value="affine">Affine Cipher</option>
             <option value="playfair">Playfair Cipher</option>
             <option value="product">Product Cipher</option>
-            <option value="file">File</option>
           </select>
 
           <div className="flex flex-col gap-4">{inputSection(type)}</div>
